41789a406fdbd10357cff0aa1235aee51ff88806
vitalya0701 <vitalik.kys.0701@gmail.com>
2019-03-29 10:36:39 +0200

    add .

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+@section('header')
+	<head>
+	 <meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	 <title>Ereaders Home Page</title>
+
+	 <!-- Css Files -->
+	 <link rel="stylesheet" href="{{asset('css/bootstrap.css') }}">
+	 <link rel="stylesheet" href="{{asset('assets/css/font-awesome.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/css/slick-slider.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/css/fancybox.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/css/color.css')}}">
+	 <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+
+	 
+
+	 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	 <!--[if lt IE 9]>
+		 <script src="../../../https@oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		 <script src="../../../https@oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	 <![endif]-->
+	</head>
+	<body>
+	 
+	 <!--// Main Wrapper \\-->
+	 <div class="ereaders-main-wrapper">
+
+	<!--// Header \\-->
+	<header id="ereaders-header" class="ereaders-header-one">
+		<div class="ereaders-main-header">
+			<div class="container">
+				<div class="row">
+					<aside class="col-md-3"> <a href="index.html" class="logo"><img src="images/logo.png" alt=""></a> </aside>
+					<aside class="col-md-9">
+						<!--// Navigation \\-->
+						<a href="#menu" class="menu-link active"><span></span></a>
+						<nav id="menu" class="menu navbar navbar-default">
+							<ul class="level-1 navbar-nav">
+								<li class="active"><a href="index.html">Головна</a></li>
+								<li><a href="#">наш блок</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+									<ul class="sub-menu level-2">
+										<li><a href="blog-grid.html">Наш блок1</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="blog-grid-wls.html">Blog Grid W/L/S</a></li>
+												<li><a href="blog-grid-wrs.html">Blog Grid W/R/S</a></li>
+											</ul>
+										</li>
+										<li><a href="blog-large.html">Blog Large</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="blog-large-wls.html">Blog Large W/L/S</a></li>
+											</ul>
+										</li>
+										<li><a href="blog-detail.html">Blog Detail</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="blog-detail-wls.html">Blog Detail W/L/S</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li><a href="#">Книги</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+									<ul class="sub-menu level-2">
+										<li><a href="book-grid.html">Book Grid</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="book-grid-wls.html">Book Grid W/L/S</a></li>
+												<li><a href="book-grid-wrs.html">Book Grid W/R/S</a></li>
+											</ul>
+										</li>
+										<li><a href="author-detail.html">Author Detail</a></li>
+										<li><a href="book-detail.html">Book Detail</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Events</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+									<ul class="sub-menu level-2">
+										<li><a href="event-list.html">Event List</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="event-list-wls.html">Event List W/L/S</a></li>
+											</ul>
+										</li>
+										<li><a href="event-detail.html">Event Detail</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="event-detail-wls.html">Event Detail W/L/S</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li><a href="#">Our Shop</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+									<ul class="sub-menu level-2">
+										<li><a href="shop-grid.html">Shop Grid</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="shop-grid-wls.html">Shop Grid W/L/S</a></li>
+												<li><a href="shop-grid-v2.html">Shop Grid V2</a></li>
+											</ul>
+										</li>
+										<li><a href="shop-list.html">Shop List</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+											<ul class="sub-menu">
+												<li><a href="shop-list-wls.html">Shop List W/L/S</a></li>
+											</ul>
+										</li>
+										<li><a href="shop-detail.html">Shop Detail</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Pages</a><span class="has-subnav"><i class="fa fa-angle-down"></i></span>
+									<ul class="sub-menu level-2">
+										<li><a href="about-us.html">About Us</a></li>
+										<li><a href="404.html">Error Page</a></li>
+										<li><a href="priceplan.html">Pricing Plan</a></li>
+										<li><a href="faq.html">Our FAQ</a></li>
+										<li><a href="search-result.html">Search Result</a></li>
+										<li><a href="contact-us.html">Contact Us</a></li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+						<!--// Navigation \\-->
+						<a href="404.html" class="ereaders-simple-btn ereaders-bgcolor">Download</a>
+					</aside>
+			 	</div>
+			</div>
+		</div>
+	</header>
+	<!--// Header \\-->
+
+	<!--// Main Banner \\-->
+	<div class="ereaders-banner">
+
+		<!--// Slider \\-->
+		<div class="ereaders-banner-layer">
+			<img src="extra-images/banner-1.jpg" alt="">
+			<span class="ereaders-banner-transparent"></span>
+			<div class="ereaders-banner-caption">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7">
+							<div class="ereaders-banner-text">
+								<h2>The Best <span>Books</span></h2>
+								<h3>Sale up to 30% off</h3>
+								<p>Quisque ultricies luctus nisi non dapibus. Nullam efficitur lorem non libe blandit, non rhoncus libero tristique.</p>
+								<a href="404.html" class="ereaders-simple-btn ereaders-bgcolor">Buy Now</a>
+							</div>
+						</div>
+						<div class="col-md-5">
+							<div class="ereaders-banner-thumb"><img src="extra-images/banner-thumb.png" alt=""></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ereaders-banner-layer">
+			<img src="extra-images/banner-2.jpg" alt="">
+			<div class="ereaders-banner-caption">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7">
+							<div class="ereaders-banner-text-two">
+								<h2>Showcase And Sell</h2>
+								<h3>Your e-book In Simple Way</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ull am lacinia tortor erat, eget scelerisque magna efficitur ut. Sus pendisse potenti.</p>
+								<div class="clearfix"></div>
+								<a href="404.html" class="ereaders-simple-btn ereaders-bgcolor">Buy Now</a>
+							</div>
+						</div>
+						<div class="col-md-5">
+							<div class="ereaders-banner-thumb-two"><img src="extra-images/banner-thumb2.png" alt=""></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--// Slider \\-->
+		 
+	</div>
+	<!--// Main Banner \\-->
+
+	<!--// Main Content \\-->
+	<div class="ereaders-main-content ereaders-content-padding">
+@show
+		<!--// Main Section \\-->
+		
+        <!--// Main Section \\-->
+@section('footer')
+	</div>
+	<!--// Main Content \\-->
+
+	<!--// Footer \\-->
+	<footer id="ereaders-footer" class="ereaders-footer-one">	 
+
+		<!--// Footer Widget \\-->
+		<div class="ereaders-footer-widget">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-12">
+						<div class="ereaders-footer-newslatter">
+							<h2>Subscribe to Our Newsletter</h2>
+							<p>Aenean ultricies iaculis cursus. Mauris enim tellus, finibus in felis a, sollicitudin iaculis dolor. Donec sollicitudin orci id efficitur dapibus.</p>
+							<form>
+								<input value="Enter Your Email Address" onblur="if(this.value == '') { this.value ='Enter Your Email Address'; }" onfocus="if(this.value =='Enter Your Email Address') { this.value = ''; }" tabindex="0" type="email">
+								<input type="submit" value="Subscribe">
+							</form>
+							<a href="index.html" class="ereaders-footer-logo"><img src="images/footer-logo.png" alt=""></a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!--// Footer Widget \\-->
+
+		<!--// CopyRight \\-->
+		<div class="ereaders-copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p><i class="fa fa-copyright"></i> 2017, All Right Reserved - by <a href="index.html">EyeCix</a></p>
+						<ul class="footer-social-network">
+							<li><a href="../../../https@en-gb.facebook.com/login/default.htm" class="fa fa-facebook"></a></li>
+							<li><a href="../../../https@www.linkedin.com/uas/login" class="fa fa-linkedin"></a></li>
+							<li><a href="../../../https@twitter.com/login@lang=en" class="fa fa-twitter"></a></li>
+							<li><a href="../../../https@www.pinterest.com/login/default.htm" class="fa fa-pinterest-p"></a></li>
+							<li><a href="../../../https@en-gb.facebook.com/login/default.htm" class="fa fa-google-plus"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>		
+		<!--// CopyRight \\-->
+
+	</footer>
+	<!--// Footer \\-->
+
+	<div class="clearfix"></div>
+
+	</div>
+	<!--// Main Wrapper \\-->
+
+	 <!-- jQuery (necessary for JavaScript plugins) -->
+	 <script type="text/javascript" src="script/jquery.js"></script>
+	 <script type="text/javascript" src="script/jquery-ui.js"></script>
+	 <script type="text/javascript" src="script/bootstrap.min.js"></script>
+	 <script type="text/javascript" src="script/slick.slider.min.js"></script>
+	 <script type="text/javascript" src="script/fancybox.pack.js"></script>
+	 <script type="text/javascript" src="script/isotope.min.js"></script>
+	 <script type="text/javascript" src="script/progressbar.js"></script>
+	 <script type="text/javascript" src="script/jquery.countdown.min.js"></script>
+	 <script type="text/javascript" src="script/circle-chart.js"></script>
+	 <script type="text/javascript" src="script/numscroller.js"></script>
+	 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js@key=AIzaSyAs_m2ywm-bE8z9YM_WmtPaIQNIosN4Dxo&callback=initMap"></script>
+	 <script type="text/javascript" src="script/functions.js"></script>
+
+	</body>
+</html>
+@show
